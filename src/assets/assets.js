6161b8e32b4ed549c756af6fee305af48fab1ba2
abhishekjadhav0505 <abhishekjadhav7272@gmail.com>
2025-11-14 12:40:39 +0530

    commit changes

---
+++ src/assets/assets.js
@@ -0,0 +1,110 @@
+import { FaLightbulb, FaPaintBrush, FaCode, FaReact, FaServer, FaTools, FaDatabase } from 'react-icons/fa';
+import { SiFuturelearn } from "react-icons/si";
+
+
+import profile from '../assets/profile.jpg';
+import project1 from '../assets/project1.png';
+import project2 from '../assets/project2.png';
+import project3 from '../assets/project3.png';
+import project4 from '../assets/project4.png';
+
+
+export const assets = {
+    profile,
+}
+
+export const aboutInfo = [
+    {
+      icon: FaLightbulb,
+      title: 'Innovative',
+      description: 'I love creating unique solutions to complex problems with cutting-edge technologies.',
+      color: 'text-purple'
+    },
+    {
+      icon: FaPaintBrush,
+      title: 'Design Oriented',
+      description: 'Beautiful design and user experience are at the heart of everything I create.',
+      color: 'text-pink'
+    },
+    {
+      icon: FaCode,
+      title: 'Clean Code',
+      description: 'I write maintainable, efficient code following best practices and modern patterns.',
+      color: 'text-blue'
+    },
+    {
+      icon: SiFuturelearn,
+      title: 'Learning',
+      description: 'A commitment to continuous learning and staying updated with industry trends.',
+      color: 'text-pink'
+    }
+
+  ];
+
+
+
+export const skills = [
+  {
+    title: 'Frontend Development',
+    icon: FaReact,
+    description: 'Building responsive and interactive user interfaces with modern frameworks.',
+    tags: ['React', 'Tailwind CSS','Bootstrap','React-Bootstrsap','Framer-motion']
+  },
+  {
+    title: 'Backend Development',
+    icon: FaServer,
+    description: 'Creating robust server-side applications and RESTful APIs.',
+    tags: ['Node.js', 'Express']
+  },
+  {
+    title: 'Database Management',
+    icon: FaDatabase,
+    description: 'Designing and optimizing databases for performance and scalability.',
+    tags: ['MongoDB']
+  },
+  {
+    title: 'Tools & Technologies',
+    icon: FaTools,
+    description: 'Essential tools and technologies I use in my development workflow.',
+    tags: ['Git & GitHub','Figma' ]
+  }
+];
+
+
+
+export const projects = [
+  {
+    title: "Food-Delivery App",
+    description: "A fully responsive food delivery web that allows users to view menus, add to cart and place orders seamlessly..",
+    image: project1,
+    tech: ["React", "CSS"], 
+    code: "https://github.com/abhishekjadhav0505/Food-Delivery02",
+    demo: "https://food-delivery0123.netlify.app"
+  },
+  {
+    title: "Real-Estate Website",
+    description: "A responsive real estate website designed to explore property listings with ease.",
+    image: project2,
+    tech: ["React","Tailwind-CSS","Framer-Motion"],
+    code: "https://github.com/abhishekjadhav0505/Real-estate01",
+    demo: "https://real-estate0123.netlify.app"
+  },
+  {
+    title: "Interior-Designing Website",
+    description: "A modern interior design website showcasing elegant layouts, design inspirations, and creativity.",
+    image: project3,
+    tech: ["React","Tailwind-CSS"],
+    code: "https://github.com/abhishekjadhav0505/Interior-Designing01",
+    demo: "https://interior-designing0123.netlify.app"
+  },
+  {
+    title: "Portfolio Website",
+    description: "A personal portfolio to showcase projects, skills, myself and contact me.",
+    image: project4,
+    tech: ["React", "Tailwind CSS", "Framer Motion"],
+    code: "#",
+    demo: "#"
+  }
+];
+
+
---
+++ src/components/Contact.jsx
@@ -0,0 +1,111 @@
+import React from 'react'
+import {motion} from 'framer-motion'
+import { FaEnvelope, FaGithub,  FaMapMarkerAlt, FaPhone } from 'react-icons/fa'
+
+const Contact = () => {
+  return (
+    <motion.div
+    initial={{opacity:0,y:50}}
+     whileInView={{opacity:1,y:0}}
+     transition={{duration:1,ease:'easeOut'}}
+     viewport={{once:false,amount:0.2}}
+     id='contact'
+     className='py-20 bg-dark-200 '>
+
+        <div className='container mx-auto px-6'>
+            <h2 className='text-3xl font-bold text-center mb-4'>Get In 
+                <span className='text-purple'>Touch</span>
+            </h2>
+            <p className='text-gray-400 text-center max-w-2xl mx-auto mb-16'>Have a project in mind or want to collaborate let's talk!</p>
+
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-12 max-w-5xl mx-auto'>
+                <div>
+                    <form className='space-y-6'>
+                        <div>
+                            <label htmlFor="name" className='block text-gray-300 mb-2'>
+                                Your Name
+                            </label>
+                            <input className='w-full bg-dark-300 border border-dark-400 rounded-lg px-4 py-3 outline-none' type="text" />
+
+                        </div>
+
+                        <div>
+                            <label htmlFor="email" className='block text-gray-300 mb-2'>
+                                Email Address
+                            </label>
+                            <input className='w-full bg-dark-300 border border-dark-400 rounded-lg px-4 py-3 outline-none' type="text" />
+
+                        </div>
+
+                        <div>
+                            <label htmlFor="message" className='block text-gray-300 mb-2'>
+                                Your Message
+                            </label>
+                            <textarea 
+                            className='w-full h-40 bg-dark-300 border border-dark-400 rounded-lg px-4 py-3 outline-none' type="text" />
+
+                        </div>
+
+                        <button type='submit' className='w-full px-6 py-3 bg-purple rounded-lg font-medium hover:bg-purple-700 transition duration-300 cursor-pointer'>
+                            Send
+
+                        </button>
+                    </form>
+                </div>
+
+                <div className='space-y-8 '>
+                    <div className='flex items-start'>
+                        <div className='text-purple text-2xl mr-4 '>
+                            <FaMapMarkerAlt/>
+                        </div>
+                        <div>
+                            <h3 className='text-lg font-semibold mb-2'>Location</h3>
+                            <p className='text-gray-400'>Pune,Maharashtra</p>
+                        </div>
+                    </div>
+
+                    <div className='flex items-start'>
+                        <div className='text-purple text-2xl mr-4 '>
+                            <FaEnvelope/>
+                        </div>
+                        <div>
+                            <h3 className='text-lg font-semibold mb-2'>Email</h3>
+                            <a href="mailto:abhishekjadhav7272@gmail.com" className='text-gray-400'>abhishekjadhav7272@gmail.com</a>
+                         </div>   
+                        
+                        </div>
+
+                     <div className='flex items-start'>
+                          <div className='text-purple text-2xl mr-4 '>
+                              <FaPhone/>
+                           </div>
+                           <div>
+                              <h3 className='text-lg font-semibold mb-2'>Phone</h3>
+                              <p className='text-gray-400'>+91 9284487282</p>
+                           </div>
+                     </div>
+
+                     <div className='pt-4'>
+                        <h3 className='text-lg font-semibold mb-4'>Follow Me</h3>
+                        <div>
+                            <a href="https://github.com/abhishekjadhav0505" className='w-12 h-12 rounded-full bg-dark-300 flex items-center justify-center text-purple hover:bg-purple hover:text-white transition duration-300'>
+                                <FaGithub/>
+                            </a>
+                                                        
+                        </div>
+
+                        
+                     </div>
+
+                </div>
+                
+                
+
+            </div>
+        </div>
+      
+    </motion.div>
+  )
+}
+
+export default Contact
